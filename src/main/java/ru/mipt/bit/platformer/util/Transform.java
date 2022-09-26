c40904ae7d8ba4fc614dd8fa0b52b370693bf1dd
AlexRuMath <bioshock20129@gmail.com>
2022-09-26 16:00:38 +0300

    add: Transform class

---
+++ src/main/java/ru/mipt/bit/platformer/util/Transform.java
@@ -0,0 +1,30 @@
+package ru.mipt.bit.platformer.util;
+
+import com.badlogic.gdx.math.GridPoint2;
+
+public class Transform {
+    private GridPoint2 position;
+    private float rotation;
+
+    public Transform(GridPoint2 position, float rotation){
+        this.position = position;
+        this.rotation = rotation;
+    }
+
+    public void copyFromTransform(Transform transform){
+        this.position = new GridPoint2(transform.getPosition());
+        this.rotation = transform.getRotation();
+    }
+
+    public GridPoint2 getPosition() {
+        return position;
+    }
+
+    public float getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(float rotation) {
+        this.rotation = rotation;
+    }
+}
