e79e25f3aaf9eb7d2c81613434d255d921be5994
AlexRuMath <bioshock20129@gmail.com>
2022-10-17 23:19:02 +0300

    update: Naming

--- src/main/java/ru/mipt/bit/platformer/level/IFillLevel.java
+++
@@ -1,8 +0,0 @@
-package ru.mipt.bit.platformer.level;
-
-import com.badlogic.gdx.math.GridPoint2;
-
-public interface IFillLevel {
-    ILevelObstacle fill();
-    GridPoint2 getPlayerPosition();
-}
