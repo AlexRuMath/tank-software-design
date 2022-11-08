b8c27bab84fef08f109fc56e8c004d1033b54c8b
AlexRuMath <bioshock20129@gmail.com>
2022-11-08 11:54:20 +0300

    update: Add method in Direction

--- src/main/java/ru/mipt/bit/platformer/util/Direction.java
+++ src/main/java/ru/mipt/bit/platformer/util/Direction.java
@@ -5,24 +5,24 @@ import com.badlogic.gdx.math.GridPoint2;
 import java.util.Random;
 
 public class Direction {
-    private final Transform direction;
+    public final Transform transform;
 
     public static Direction Up = new Direction(new GridPoint2(0, 1), 90f);
     public static Direction Down = new Direction(new GridPoint2(0, -1), -90f);
     public static Direction Left = new Direction(new GridPoint2(-1, 0), 180f);
     public static Direction Right = new Direction(new GridPoint2(1, 0), 0f);
 
-    public Direction(GridPoint2 direction, float rotation) {
-        this.direction = new Transform(direction, rotation);
+    public Direction(GridPoint2 transform, float rotation) {
+        this.transform = new Transform(transform, rotation);
     }
 
     public Transform stepInTheDirection(Transform startTransform){
         GridPoint2 newPosition = new GridPoint2(
-                this.direction.getPosition().x + startTransform.getPosition().x,
-                this.direction.getPosition().y + startTransform.getPosition().y
+                this.transform.position.x + startTransform.position.x,
+                this.transform.position.y + startTransform.position.y
         );
 
-        return new Transform(newPosition, this.direction.getRotation());
+        return new Transform(newPosition, this.transform.rotation);
     }
 
     public static Direction getRandom(){
@@ -43,5 +43,25 @@ public class Direction {
                 break;
         }
         return randomDirection;
+    }
+    
+    public static Direction fromRotation(float rotation){
+        if(rotation == 0f){
+            return Direction.Right;
+        }
+
+        if(rotation == 90f){
+            return Direction.Up;
+        }
+
+        if(rotation == 180f){
+            return Direction.Left;
+        }
+
+        if(rotation == -90f){
+            return Direction.Down;
+        }
+
+        return null;
     }
 }
